49528658964a2183c5c37287a325db4121803aaa
Matthew Schaefer <mschaefer@electrainfo.com>
2017-03-17 11:56:47 -0400

    update

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/levigross/grequests"
+)
+
+func requestSomething() {
+	resp, err := grequests.Get("http://httpbin.org/get", nil)
+	// You can modify the request by passing an optional RequestOptions struct
+
+	if err != nil {
+		fmt.Println("Unable to make request: ", err)
+	}
+	fmt.Println(resp.String())
+}
 
 func sayAThing() {
 	fmt.Println("say a thing")
@@ -9,4 +23,5 @@ func sayAThing() {
 func main() {
 	fmt.Println("hello world")
 	sayAThing()
+	requestSomething()
 }
